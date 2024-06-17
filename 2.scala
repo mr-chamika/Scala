647adceb98fa1175cbb28c99c4164f7c5be76403
mr-chamika <140165336+mr-chamika@users.noreply.github.com>
2024-06-17 08:15:12 +0530

    Update 2.scala

--- 2.scala
+++ 2.scala
@@ -1,11 +1,3 @@
-object main{
-  
-  def c2f(c:Double)=(c*1.8)+32.00
-
-  def main(args:Array[String])={
+def c2f(c:Double)=(c*1.8)+32.00
     
-    println("Fahrenheit " + c2f(35))
-
-  }
-
-}
+println("Fahrenheit " + c2f(35))
