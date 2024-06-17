b3fba295df32825c1738bc5cf959e0fa472a464f
mr-chamika <140165336+mr-chamika@users.noreply.github.com>
2024-06-17 08:15:44 +0530

    Update 3.scala

--- 3.scala
+++ 3.scala
@@ -1,11 +1,3 @@
-object main{
-  
-  def vol(r:Int)=(4/3)*math.Pi*r*r*r
+def vol(r:Int)=(4/3)*math.Pi*r*r*r
 
-  def main(args:Array[String])={
-
-    println("Volume of the sphere is = " + vol(5))
-
-  }
-
-}
+println("Volume of the sphere is = " + vol(5))
