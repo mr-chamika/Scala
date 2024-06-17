f337780b9eae3668d503fa45cfc7b6ad82e8e949
mr-chamika <140165336+mr-chamika@users.noreply.github.com>
2024-06-17 08:16:12 +0530

    Update 4.scala

--- 4.scala
+++ 4.scala
@@ -1,11 +1,5 @@
-object main{
-
-  def shippingCost(a:Int):Double=if(a<=50) a*3 else (a-50)*0.75 + 50*3
-  def discount(x:Int,y:Double):Double=x*y*0.4
-  def TotalCoverPrice(l:Int,m:Double)=l*m
+def shippingCost(a:Int):Double=if(a<=50) a*3 else (a-50)*0.75 + 50*3
+def discount(x:Int,y:Double):Double=x*y*0.4
+def TotalCoverPrice(l:Int,m:Double)=l*m
   
-  def main(args:Array[String])={
-    println("Total wholesale cost is = Rs. " + (TotalCoverPrice(60,24.95) + shippingCost(60) - discount(60,24.95)))
-  }
-
-}
+println("Total wholesale cost is = Rs. " + (TotalCoverPrice(60,24.95) + shippingCost(60) - discount(60,24.95)))
