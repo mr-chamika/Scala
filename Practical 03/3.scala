8e1ed4b0a670cfa33d3874a47147654b00fdcb83
mr-chamika <140165336+mr-chamika@users.noreply.github.com>
2024-07-02 10:50:07 +0530

    Update 3.scala

--- Practical 03/3.scala
+++ Practical 03/3.scala
@@ -1,7 +1,7 @@
-val num1 = 5.4
-val num2 = 8.9
+val num1 = 5
+val num2 = 8
 
-def Mean(num1:Double, num2:Double):Float = BigDecimal((num1 + num2)/2).setScale(2,BigDecimal.RoundingMode.HALF_UP).toFloat
+def Mean(num1:Int, num2:Int):Float = BigDecimal((num1 + num2)/2).setScale(2,BigDecimal.RoundingMode.HALF_UP).toFloat
           
 val x = Mean(num1,num2)  
 println(f"$x%.2f")
